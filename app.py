1b57b54a614eed6f268da64babe17ae42b3e30db
ACHRAF ZHAR <achrafzhar2@gmail.com>
2024-05-25 16:47:51 +0100

    add lfs

--- app.py
+++ app.py
@@ -12,11 +12,10 @@ net_ESRGAN_path = './models/RRDB_ESRGAN_x4.pth'
 net_PSNR = torch.load(net_PSNR_path)
 net_ESRGAN = torch.load(net_ESRGAN_path)
 
-# Set device to CUDA if available, otherwise CPU
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Load the YOLOv8 model
-model_yolo = YOLO("./models/yolov8n-face.pt")  # Replace with your model path
+model_yolo = YOLO("./models/yolov8n-face.pt") 
 
 def detecte_enhance_faces(image, alpha):
     net_interp = OrderedDict()
@@ -25,30 +24,23 @@ def detecte_enhance_faces(image, alpha):
         v_ESRGAN = net_ESRGAN[k]
         net_interp[k] = (1 - alpha) * v_PSNR + alpha * v_ESRGAN
 
-    # Initialize and load the model
     model = arch.RRDBNet(3, 3, 64, 23, gc=32)
     model.load_state_dict(net_interp, strict=True)
     model.eval()
     model_esrgan = model.to(device)
 
-    # Convert the input image from RGB to BGR (OpenCV format)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    # Perform object detection
     results = model_yolo.predict(source=image, stream=True)
 
-    # List to hold detected face images
     enhanced_face_images = []
 
-    # Iterate through the detected objects
     for result in results:
-        boxes = result.boxes  # Bounding boxes for detected objects
+        boxes = result.boxes  
         for box in boxes:
-            # Extract bounding box coordinates
-            x1, y1, x2, y2 = box.xyxy[0]  # Box coordinates (x1, y1, x2, y2)
+            x1, y1, x2, y2 = box.xyxy[0]  
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
-            # Extract the RoI (Region of Interest) from the image
             img = image[y1:y2, x1:x2]
             img = img * 1.0 / 255
             img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
@@ -60,15 +52,12 @@ def detecte_enhance_faces(image, alpha):
             output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
             output = (output * 255.0).round().astype(np.uint8)
 
-            # Convert the RoI back to RGB format
             roi = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
 
-            # Add the detected face to the list
             enhanced_face_images.append(roi)
 
     return enhanced_face_images
 
-# Define the Gradio interface
 iface = gr.Interface(
     fn=detecte_enhance_faces,
     inputs=[
@@ -81,5 +70,4 @@ iface = gr.Interface(
     flagging_options=None 
 )
 
-# Launch the Gradio app
 iface.launch()
